429ed4b9db32fa477f480ce4c5007c6485fcd6d1
kaloyanTry <kaloyantryavna@gmail.com>
2025-01-14 17:06:14 +0200

    Finalising autentication functioality sign-up

--- lib/constants/validators.ts
+++ lib/constants/validators.ts
@@ -28,3 +28,15 @@ export const signInFormSchema = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
+
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(2, 'Name must be at least 2 characters.'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: z.string().min(6, 'Confirm password'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ['confirmPassword'],
+  });
