7f1daaee999f6a0d122c678b3ac073c44844fcb3
kaloyanTry <kaloyantryavna@gmail.com>
2025-01-15 20:24:46 +0200

    Prisma Schema Cart added

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/constants/validators';
+import {
+  insertProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from '@/lib/constants/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
