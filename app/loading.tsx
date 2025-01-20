7dba4caee7576cb41c66e05fdd9498f03299bb67
kaloyanTry <kaloyantryavna@gmail.com>
2025-01-20 20:24:16 +0200

    Unoptimazed image nextjs

--- app/loading.tsx
+++ app/loading.tsx
@@ -12,7 +12,13 @@ const LoadingPage = () => {
         width: '100vw',
       }}
     >
-      <Image src={loader} alt='Loading...' width={150} height={150} />
+      <Image
+        src={loader}
+        alt='Loading...'
+        width={150}
+        height={150}
+        unoptimized={true}
+      />
     </div>
   );
 };
